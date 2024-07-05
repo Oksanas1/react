dcaa634d6824dadd8bead304c46d7c9ca2287fce
Oksana <benefit989@gmail.com>
2024-07-05 18:25:51 +0200

    first commit

---
+++ lesson2/clw2/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+
+const rootElem = document.querySelector('#root');
+const root = ReactDOM.createRoot(rootElem);
+
+root.render(
+<main className="page">
+  <form className="login-form">
+    <h1 className="form-title">Profile</h1>
+    <div className="form-control">
+      <label className="form-label" htmlFor="name">Name</label>
+      <input className="form-input" type="text" id="name" name="name" defaultValue="anonymous" />
+    </div>
+    <div className="form-control">
+      <label className="form-label" id="age" htmlFor="age">Age</label>
+      <input className="form-input" type="number" defaultValue="17" name="age" />
+      <span style={{ color: 'red', fontWeight: '700' }}>To young</span>
+    </div>
+    <button className="submit-button" type="submit">Submit</button>
+  </form>
+</main>
+);

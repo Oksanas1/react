8dfc3223cae4edc80661d1defa63f4e3da10b3ea
Oksana <benefit989@gmail.com>
2024-07-05 16:16:50 +0200

    first commit

---
+++ lesson1/todo-list/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const rootElem = document.querySelector('#root');
+
+const root = ReactDOM.createRoot(rootElem);
+root.render(
+  <h1 className="title">Todo List</h1>
+);

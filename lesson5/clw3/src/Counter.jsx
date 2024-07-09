be643246e12e96d6aa4689ba628386eb1dc482d1
Oksana <benefit989@gmail.com>
2024-07-09 16:32:41 +0200

    fix counter

--- lesson5/clw3/src/Counter.jsx
+++ lesson5/clw3/src/Counter.jsx
@@ -9,6 +9,8 @@ export default class Counter extends Component {
     }
 
     this.handleClick = this.handleClick.bind(this);
+    this.addCounter = this.addCounter.bind(this);
+    this.decreaseCounter = this.decreaseCounter.bind(this);
   }
 
   addCounter() {
@@ -16,23 +18,35 @@ export default class Counter extends Component {
       count: this.state.count + 1,
     })
   }
-
+  
   decreaseCounter() {
     this.setState({
       count: this.state.count - 1,
     })
   }
 
+  resetCounter() {
+    this.setState({
+      count: 0,
+    })
+  }
+
   handleClick(event) {
-    event.target.dataset.action === "increase" ? this.addCounter() : this.decreaseCounter();
+    // if(event.target.dataset.action === "decrease") {
+    //   this.decreaseCounter();
+    // } else if(event.target.dataset.action === "increase") {
+    //   this.addCounter();
+    // }
+
+    this.resetCounter();
   }
   
   render() {
     return (
       <div className="counter">
-        <button data-action="decrease" className="counter__button" onClick={ this.handleClick }>-</button>
-        <span className="counter__value">{this.state.count}</span>
-        <button data-action="increase" className="counter__button" onClick={ this.handleClick }>+</button>
+        <button data-action="decrease" className="counter__button" onClick={ this.decreaseCounter }>-</button>
+        <span className="counter__value" onClick={ this.handleClick }>{this.state.count}</span>
+        <button data-action="increase" className="counter__button" onClick={ this.addCounter }>+</button>
       </div>    
     );
   }

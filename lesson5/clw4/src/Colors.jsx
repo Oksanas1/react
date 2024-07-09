fe75110b204a789a5a5faa8d2b3099b7ba146bde
Oksana <benefit989@gmail.com>
2024-07-09 21:58:56 +0200

    fix

--- lesson5/clw4/src/Colors.jsx
+++ lesson5/clw4/src/Colors.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 
 
 export default class Counter extends Component {
-  handleClick = (color, event) => {
-    document.body.style.background = color;
+  setBodyColor = (color, event) => {
+    document.body.style.backgroundColor = color;
   }
   
   render() {
     return (
       <div className='colors'>
-        <button className="colors__button" onClick={ this.handleClick.bind(this, "red") } style={{background: "red"}}></button>
-        <button className="colors__button" onClick={ this.handleClick.bind(this, "green") } style={{background: "green"}}></button>
-        <button className="colors__button" onClick={ this.handleClick.bind(this, "blue") } style={{background: "blue"}}></button>
+        <button className="colors__button" onClick={ this.setBodyColor.bind(this, "#f00") } style={{background: "#f00"}}></button>
+        <button className="colors__button" onClick={ this.setBodyColor.bind(this, "#0f0") } style={{background: "#0f0"}}></button>
+        <button className="colors__button" onClick={ this.setBodyColor.bind(this, "#00f") } style={{background: "#00f"}}></button>
       </div>
     );
   }

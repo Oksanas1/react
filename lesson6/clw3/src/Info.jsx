2f6200e288c2b6e33c45f0d477884876437f1655
Oksana <benefit989@gmail.com>
2024-07-11 14:57:30 +0200

    fix

--- lesson6/clw3/src/Info.jsx
+++ lesson6/clw3/src/Info.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default function Info({info}) {
+  if(info.length === 0) {
+    return null;
+  }
+
   return (
     <div className="message">
       <p className="info">{info}</p>
--- lesson6/clw3/src/Page.jsx
+++ lesson6/clw3/src/Page.jsx
@@ -3,7 +3,7 @@ import Info from './Info';
 
 export default class Page extends Component {
   state = {
-    info: null,
+    info: '',
   }
 
   handleClick(info) {
@@ -15,7 +15,7 @@ export default class Page extends Component {
   render() {
     return (
       <div className="page">
-        {this.state.info && <Info info={this.state.info} />}
+        <Info info={this.state.info} />
         <div className="actions">
           <button className="btn" onClick={this.handleClick.bind(this, 'Price is 500$. Available in 2 colors')}>IPhone 13</button>
           <button className="btn" onClick={this.handleClick.bind(this, 'Price is 650$. Not available')}>IPhone 13 Pro</button>

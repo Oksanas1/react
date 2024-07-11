2215dffae5f94030ab8da173f1bcfbc2230fdfa3
Oksana <benefit989@gmail.com>
2024-07-11 14:46:35 +0200

    first commit

---
+++ lesson6/clw3/src/Info.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Info({info}) {
+  return (
+    <div className="message">
+      <p className="info">{info}</p>
+    </div>);
+}
---
+++ lesson6/clw3/src/Page.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Info from './Info';
+
+export default class Page extends Component {
+  state = {
+    info: '',
+  }
+
+  handleClick(info) {
+    this.setState({
+      info,
+    });
+  }
+
+  render() {
+    return (
+      <div className="page">
+        {this.state.info.length > 0 && <Info info={this.state.info} />}
+        <div className="actions">
+          <button className="btn" onClick={this.handleClick.bind(this, 'IPhone 13 - Price is 500$. Available in 2 colors')}>IPhone 13</button>
+          <button className="btn" onClick={this.handleClick.bind(this, 'Price is 650$. Not available')}>IPhone 13 Pro</button>
+          <button className="btn" onClick={this.handleClick.bind(this, '')}>Clear</button>
+        </div>
+      </div>
+    );
+  };
+}

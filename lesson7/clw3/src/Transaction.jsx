ae49da815cdecbc7268b24eb114c842c148591bc
Oksana <benefit989@gmail.com>
2024-07-15 14:07:25 +0200

    fix

--- lesson7/clw3/src/Transaction.jsx
+++ lesson7/clw3/src/Transaction.jsx
@@ -7,7 +7,7 @@ export default function Transaction({time, from, to, rate, amount}) {
   return (
     <li className="transaction">
       <span className="transaction__date">{moment(timeDate).format('DD MMM')}</span>
-      <span className="transaction__time">{moment(timeDate).utc().format('HH:mm')}</span>
+      <span className="transaction__time">{moment(timeDate).format('HH:mm')}</span>
       <span className="transaction__assets">{from} → {to}</span>
       <span className="transaction__rate">{rate}</span>
       <span className="transaction__amount">{new Intl.NumberFormat('en-GB').format(amount)}</span>

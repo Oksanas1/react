bca416e0a36e42e9e773d211c00e398a00100c08
Oksana <benefit989@gmail.com>
2024-07-17 16:54:11 +0200

    fix

--- lesson9/clw2/src/App.jsx
+++ lesson9/clw2/src/App.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import UserForm from "./UserForm";
 
 export default class App extends React.Component {
-  createUser = (event) => {
-    event.preventDefault();
-    const formData = [...new FormData(event.target)].reduce((acc,[name, value]) => ({...acc, [name]: value}), {});
-
-    console.log(formData)
+  createUser = (data) => {
+    console.log(data);
   }
 
   render() {

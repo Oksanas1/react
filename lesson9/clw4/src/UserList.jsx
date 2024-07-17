c39c60b4a65009a1fe99d5eafb10c51e0f2e0f1c
Oksana <benefit989@gmail.com>
2024-07-17 21:49:54 +0200

    first commit

---
+++ lesson9/clw4/src/UserList.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import User from "./User";
+import Filter from "./Filter";
+
+export default class UserForm extends Component {
+  state = {
+    filterText: '',
+  }
+  
+  handleFilterChange = event => {
+    this.setState({
+      filterText: event.target.value,
+    });
+  };
+
+  render() {
+    const filteredUsers = (this.state.filterText === '') 
+      ? this.props.users
+      : this.props.users.filter(user =>
+        user.name.toLowerCase().includes(this.state.filterText.toLowerCase()));
+
+    return (
+      <div>
+        <Filter filterText={this.state.filterText} count={filteredUsers.length} onChange={this.handleFilterChange} />
+        <ul className="users">
+          {filteredUsers.map(({id, name, age}) => (
+            <User key={id} name={name} age={age} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+};
---
+++ lesson9/clw4/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import UserList from './UserList';
+
+const generateUsers = () => {
+  const users = [];
+  for (let i = 1; i < 11; i++) {
+    users.push({
+      id: i,
+      name: `User${i}`,
+      age: Math.floor(Math.random() * 50) + 18,
+    });
+  }
+  return users;
+};
+
+const rootElem = document.querySelector('#root');
+const root = ReactDOM.createRoot(rootElem);
+root.render(<UserList users={generateUsers()}/>);

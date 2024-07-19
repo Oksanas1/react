a707b49f81feb9584ef5f2440365d28b39f87ea3
Oksana <benefit989@gmail.com>
2024-07-19 19:11:03 +0200

    fix

--- lesson10/clw4/src/ProductRow.jsx
+++ lesson10/clw4/src/ProductRow.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const ProductRow = ({ product }) => {
   const {name, price, stocked} = product;
-  const nameProduct = stocked ? name : (
-    <span style={{ color: 'red' }}>{name}</span>);
+  const nameProduct = stocked ? name :
+    <span style={{ color: 'red' }}>
+      {name}
+    </span>;
 
   return (
     <tr>

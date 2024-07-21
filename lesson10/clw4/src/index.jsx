ce9bf9076d5f5e8e0607e19a01695b593573efac
Oksana <benefit989@gmail.com>
2024-07-21 18:35:40 +0200

    fix

--- lesson10/clw4/src/index.jsx
+++ lesson10/clw4/src/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import App from './App';
+import FilterableProductTable from "./FilterableProductTable";
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
 
 const rootElem = document.querySelector('#root');
 const root = ReactDOM.createRoot(rootElem);
-root.render(<App />);
+root.render(<FilterableProductTable products={PRODUCTS} />);

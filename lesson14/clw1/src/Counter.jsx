82e4444b6f6b0ac3f782b332778bc2bd8d31a506
Oksana <benefit989@gmail.com>
2024-07-26 13:53:10 +0200

    fix, delet func

--- lesson14/clw1/src/Counter.jsx
+++ lesson14/clw1/src/Counter.jsx
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-  const [ count, setCount] = useState(0);
-
-  function decrementCount() {
-    setCount(count - 1);
-  }
-
-  function incrementCount() {
-    setCount(count + 1);
-  }
+  const [ counter, setCounter] = useState(0);
 
   return (
     <div className="counter">
-      <button className="counter__button" onClick={decrementCount.bind(this)}>-</button>
+      <button className="counter__button" onClick={() => setCounter(counter - 1)}>-</button>
       <span className="counter__value"> {count} </span>
-      <button className="counter__button" onClick={incrementCount.bind(this)}>+</button>
+      <button className="counter__button" onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   );
 };

b6082cd5232e53a0ccc5c392429af4df5d27570a
Oksana <benefit989@gmail.com>
2024-07-26 13:58:47 +0200

    fix

--- lesson14/clw1/src/Counter.jsx
+++ lesson14/clw1/src/Counter.jsx
@@ -6,7 +6,9 @@ const Counter = () => {
   return (
     <div className="counter">
       <button className="counter__button" onClick={() => setCounter(counter - 1)}>-</button>
-      <span className="counter__value"> {counter} </span>
+      <span className="counter__value" onClick={() => setCounter(0)}>
+        {counter}
+      </span>
       <button className="counter__button" onClick={() => setCounter(counter + 1)}>+</button>
     </div>
   );
